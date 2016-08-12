1123547740d68bf56f3c53527fae0808c9a117d4
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-08-12 14:16:47 +0100

    Use pattern matching

--- addon/helpers/bytes.js
+++ addon/helpers/bytes.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
-export function bytes(params) {
-	let amnt = parseFloat(params[0]) || 0;
+export function bytes([val]) {
+	let amnt = parseFloat(val) || 0;
 	let unit = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
 	let size = parseInt( Math.floor( Math.log(amnt) / Math.log(1024) ) );
 	return Math.round( amnt / Math.pow(1024, size), 2) + ' ' + unit[size];
