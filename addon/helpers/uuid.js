5a465324623b3589ee113c4e6384b1d1245e8cdb
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-01-27 22:42:06 +0000

    Fix spacing

--- addon/helpers/uuid.js
+++ addon/helpers/uuid.js
@@ -5,10 +5,10 @@ export default Ember.Helper.extend({
 	compute() {
 		return function() {
 			let d = new Date().getTime();
-				return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-					let r = (d + Math.random()*16)%16 | 0;
-					return ( c === 'x' ? r : (r&0x3|0x8) ).toString(16);
-				});
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+				let r = (d + Math.random()*16)%16 | 0;
+				return ( c === 'x' ? r : (r&0x3|0x8) ).toString(16);
+			});
 		};
 	}
 
