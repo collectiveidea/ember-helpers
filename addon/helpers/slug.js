deb4aff4d3da611c26a72f8fd2e08523d3af7ab0
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-07-06 18:54:14 +0100

    Improve slug helper

--- addon/helpers/slug.js
+++ addon/helpers/slug.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function slug([value='']) {
-	return String(value).replace(/'/gi, '').replace(/"/gi, '').replace(/\s/gi, '-').replace(/(?![a-zA-Z0-9])/gi, '').toLowerCase();
+	return String(value).replace(/['"]/gi, '').replace(/[\s\.]/gi, '-').replace(/(?![a-zA-Z0-9])/gi, '').toLowerCase();
 }
 
 export default Ember.Helper.helper(slug);
